080d89afde1626f10611d968927c519897031660
K.H <kelvin.ho@iag.com.au>
2021-08-05 18:08:27 +1000

    add version

--- argo/generate_apps.sh
+++ argo/generate_apps.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-[[ "$1" != '' ]] && export APP_ID=$1 || export APP_ID=demo-web-app-100
-[[ "$2" != '' ]] && export BRANCH=$2 || export BRANCH=HEAD
-[[ "$3" != '' ]] && export REPOS=$3  || export REPOS=laputa
+[[ "$1" != '' ]] && export APP_ID=$1  || export APP_ID=demo-web-app-100
+[[ "$2" != '' ]] && export BRANCH=$2  || export BRANCH=HEAD
+[[ "$3" != '' ]] && export REPOS=$3   || export REPOS=laputa
+[[ "$4" != '' ]] && export VERSION=$4 || export VERSION='1.0.0'
 
 cat apps_template.yaml | \
     sed "s/{{.APP_ID}}/${APP_ID}/g" | \
     sed "s/{{.BRANCH}}/${BRANCH}/g" | \
+    sed "s/{{.VERSION}}/${VERSION}/g" | \
     sed "s/{{.REPOS}}/${REPOS}/g" | grep -v '#'
